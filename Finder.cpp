7a411636bdc66f312910aad00bfc14fb6788eaae
MankaMankaManka <109943723+MankaMankaManka@users.noreply.github.com>
2023-04-02 21:04:30 +0930

    Update Finder.cpp

--- Finder.cpp
+++ Finder.cpp
@@ -3,16 +3,18 @@
 using namespace std;
 vector<int> Finder::findSubstrings(string s1, string s2) {
 vector<int> result;
-  size_t found=0;
+size_t found=0;
 for(size_t i = 1; i <= s2.size(); i++) {
-size_t found = s1.find(s2.substr(0, i), found);
+found = s1.find(s2.substr(0, i), found);
 if (found != string::npos) {
 result.push_back(found);
+cout << found << "\n";
 } else {
-  for (size_t j=i; j<=s2.size(); j++){
+    for (size_t j=i; j<=s2.size(); j++){
 result.push_back(-1);
-  }
-  return result;
+cout <<-1 <<"\n";
+}
+return result;
 }
 }
 return result;
